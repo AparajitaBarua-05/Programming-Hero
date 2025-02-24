5a5afa9b472a9fbea0edf679d73548f03ba759b6
AparajitaBarua-05 <aparajita0117@student.nstu.edu.bd>
2025-02-24 16:24:37 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+//print anything using
+console.log("Hello World");
+//
+var Number1;
+let Number2;
+const Number3 = prompt("Enter a Number");
+console.log(`$(Number3)`);
+//*css*/
+if(typeof number3 =="number"){
+for(let i=1;i<=10;i++)
+{
+    console.log(`${Number3} x ${i} = ${Number3 * i}`);
+}
+}
+else{
+    console.log("Please enter a number");
+}
